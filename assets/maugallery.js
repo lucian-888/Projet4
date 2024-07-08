1959a207e75b7ac77353865615481de15ce64390
Lucian Bistreanu <lucianbistreanu@yahoo.com>
2024-07-08 10:12:38 +0200

    reduced images, loading=lazy

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -121,64 +121,101 @@
       $(`#${lightboxId}`).modal("toggle");
     },
     prevImage(lightboxId) {
+      // Récupérer l'image active dans la lightbox
       let activeImage = $(`#${lightboxId} .lightboxImage`);
+    
+      // Récupérer le tag actif dans la barre de tags
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+    
+      // Initialiser un tableau pour stocker les images
       let imagesCollection = [];
-      
+    
+      // Vérifier si le tag actif est "all"
       if (activeTag === "all") {
+        // Si c'est "all", parcourir toutes les colonnes d'images
         $(".item-column").each(function() {
+          // Vérifier si la colonne contient une image
           if ($(this).children("img").length) {
+            // Si oui, ajouter l'image au tableau imagesCollection
             imagesCollection.push($(this).children("img"));
           }
         });
       } else {
+        // Sinon, parcourir les colonnes d'images avec le tag actif
         $(".item-column").each(function() {
+          // Vérifier si l'image dans la colonne a le tag actif
           if ($(this).children("img").data("gallery-tag") === activeTag) {
+            // Si oui, ajouter l'image au tableau imagesCollection
             imagesCollection.push($(this).children("img"));
           }
         });
       }
-      
+    
+      // Initialiser l'index de l'image active et l'image précédente
       let index = 0, prev = null;
+    
+      // Parcourir le tableau imagesCollection pour trouver l'index de l'image active
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
         }
       });
-      
+    
+      // Calculer l'image précédente en utilisant l'index
       prev = imagesCollection[index - 1] || imagesCollection[imagesCollection.length - 1];
+    
+      // Mettre à jour la source de l'image dans la lightbox avec l'image précédente
       $(`#${lightboxId} .lightboxImage`).attr("src", $(prev).attr("src"));
     },
     
     nextImage(lightboxId) {
+      // Récupérer l'image active dans la lightbox
       let activeImage = $(`#${lightboxId} .lightboxImage`);
+    
+      // Récupérer le tag actif dans la barre de tags
       let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+    
+      // Initialiser un tableau pour stocker les images
       let imagesCollection = [];
-      
+    
+      // Vérifier si le tag actif est "all"
       if (activeTag === "all") {
+        // Si c'est "all", parcourir toutes les colonnes d'images
         $(".item-column").each(function() {
+          // Vérifier si la colonne contient une image
           if ($(this).children("img").length) {
+            // Si oui, ajouter l'image au tableau imagesCollection
             imagesCollection.push($(this).children("img"));
           }
         });
       } else {
+        // Sinon, parcourir les colonnes d'images avec le tag actif
         $(".item-column").each(function() {
+          // Vérifier si l'image dans la colonne a le tag actif
           if ($(this).children("img").data("gallery-tag") === activeTag) {
+            // Si oui, ajouter l'image au tableau imagesCollection
             imagesCollection.push($(this).children("img"));
           }
         });
       }
-      
+    
+      // Initialiser l'index de l'image active et l'image suivante
       let index = 0, next = null;
+    
+      // Parcourir le tableau imagesCollection pour trouver l'index de l'image active
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
           index = i;
         }
       });
-      
+    
+      // Calculer l'image suivante en utilisant l'index
       next = imagesCollection[index + 1] || imagesCollection[0];
+    
+      // Mettre à jour la source de l'image dans la lightbox avec l'image suivante
       $(`#${lightboxId} .lightboxImage`).attr("src", $(next).attr("src"));
     },
+    
     
     
     createLightBox(gallery, lightboxId, navigation) {
@@ -204,39 +241,58 @@
                 </div>
             </div>`);
     },
-    showItemTags(gallery, position, tags) {
-      var tagItems =
-        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
-      $.each(tags, function(index, value) {
-        tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
-      });
-      var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
+   
 
+    showItemTags(gallery, position, tags) {
+      // Initialiser la variable tagItems avec le bouton "Tous" par défaut
+      var tagItems = '<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all">Tous</span></li>';
+    
+      // Parcourir chaque tag dans le tableau tags
+      $.each(tags, function(index, value) {
+        // Ajouter un élément de liste pour chaque tag
+        tagItems += `<li class="nav-item">
+                <span class="nav-link" data-images-toggle="${value}">${value}</span></li>`;
+      });
+    
+      // Envelopper les éléments de liste dans une balise <ul>
+      var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
+    
+      // Déterminer la position des boutons de catégorie
       if (position === "bottom") {
+        // Ajouter les boutons de catégorie en bas de la galerie
         gallery.append(tagsRow);
       } else if (position === "top") {
+        // Ajouter les boutons de catégorie en haut de la galerie
         gallery.prepend(tagsRow);
       } else {
+        // Afficher une erreur si la position n'est pas valide
         console.error(`Unknown tags position: ${position}`);
       }
     },
+    
     filterByTag() {
+      // Récupérer le tag associé au bouton cliqué
       let tag = $(this).data("images-toggle");
     
-      // Remove active class from all tags
+      // Supprimer la classe active de tous les boutons
       $(".nav-link").removeClass("active-tag");
       
-      // Add active class to the clicked tag
+      // Ajouter la classe active au bouton cliqué
       $(this).addClass("active-tag");
     
+      // Vérifier si le tag est "all" (Tous)
       if (tag === "all") {
+        // Si c'est "all", afficher toutes les images
         $(".gallery-item").parents(".item-column").show();
       } else {
+        // Sinon, cacher toutes les images
         $(".gallery-item").parents(".item-column").hide();
+        
+        // Afficher uniquement les images avec le tag correspondant
         $(".gallery-item[data-gallery-tag='" + tag + "']").parents(".item-column").show();
       }
     },
+    
     
     
     
